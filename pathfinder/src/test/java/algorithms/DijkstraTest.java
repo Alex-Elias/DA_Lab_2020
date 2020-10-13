e16856d8a39d992530b1827e8e866265613d408b
Alex-Elias <alex.pohjanpelto@gmail.com>
2020-10-13 14:46:04 +0300

    created DijkstraTest

---
+++ pathfinder/src/test/java/algorithms/DijkstraTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+import datastructures.NodeList;
+import datastructures.Tuple;
+import filereader.Filereader;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alex
+ */
+public class DijkstraTest {
+    private Filereader fr;
+    
+    
+    
+    
+    @Before
+    public void setUp() {
+        this.fr = new Filereader("Mazes/test.map");
+    }
+    
+    
+
+    @Test
+    public void findPathSimpleMap(){
+        Dijkstra dijk = new Dijkstra(fr.returnMaze());
+        dijk.runDijkstra(new Tuple(0,0),new Tuple(3,0));
+        NodeList list = dijk.getShortestPath();
+        
+        assertTrue(list.length() > 0);
+    }
+    
+    @Test
+    public void findShortestPathSimpleMap(){
+        Dijkstra dijk = new Dijkstra(fr.returnMaze());
+        dijk.runDijkstra(new Tuple(0,0), new Tuple(0,3));
+        assertTrue(Math.abs(dijk.getDistance(new Tuple(0,3)) - 18.31) < 0.01);
+    }
+}
